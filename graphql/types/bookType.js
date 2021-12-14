74b741d170ac30a4495ddea411faad6ee753a045
Horjea Cosmin <horjeacosmin@gmail.com>
2021-12-15 00:24:45 +0200

    format problems

--- graphql/types/bookType.js
+++ graphql/types/bookType.js
@@ -1,42 +1,41 @@
 const {
-    GraphQLObjectType,
-    GraphQLString,
-    GraphQLID,
-    GraphQLList
-} = require('graphql');
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLID,
+  GraphQLList,
+} = require("graphql");
 
-const AuthorType = require('./authorType');
-const ReviewType = require('./reviewType');
+const AuthorType = require("./authorType");
+const ReviewType = require("./reviewType");
 
 const BookType = new GraphQLObjectType({
-    name: 'Book',
-    fields: {
-        id: {
-            type: GraphQLID
-        },
-        title: {
-            type: GraphQLString
-        },
-        description: {
-            type: GraphQLString
-        },
-        releaseDate: {
-            type: GraphQLString
-        },
-        author: {
-            type: require('./authorType'),
-            resolve: async (source) => {
-                return await source.getAuthor();
-            }
-        },
-        reviews: {
-            type: new GraphQLList(ReviewType),
-            resolve: async (source) => {
-                return await source.getReviews();
-            }
-        }
-
-    }
+  name: "Book",
+  fields: {
+    id: {
+      type: GraphQLID,
+    },
+    title: {
+      type: GraphQLString,
+    },
+    description: {
+      type: GraphQLString,
+    },
+    releaseDate: {
+      type: GraphQLString,
+    },
+    author: {
+      type: AuthorType,
+      resolve: async (source) => {
+        return await source.getAuthor();
+      },
+    },
+    reviews: {
+      type: new GraphQLList(ReviewType),
+      resolve: async (source) => {
+        return await source.getReviews();
+      },
+    },
+  },
 });
 
 module.exports = BookType;
